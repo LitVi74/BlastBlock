3838249c7bcc156338344047415238ab3a610e1d
LitVi74 <71370749+LitVi74@users.noreply.github.com>
2021-05-12 15:20:52 +0500

    add Phaser. Part 2

--- js/canvas.js
+++ js/canvas.js
@@ -5,8 +5,12 @@ export class Canvas{
     create () {
         this.cols_ = 0;
         this.rows_ = 0;
+        this.graphics_ = this.add.graphics();
+
         let cells_ = this.game.field_.fill();
         this.draw_(cells_);
+        
+        this.input.on('pointerdown', this.onClick_, this);
     }
 
     draw_(field) { // закрвшиваем поле
@@ -24,19 +28,20 @@ export class Canvas{
             this.cols_ = field[0].length;
         }
 
-        let graphics = this.add.graphics();
+        
 
         for (let x = 0; x < this.rows_; x++) {
             for (let y = 0; y < this.cols_; y++) {
-                this.drawTile_(field[x][y].position, field[x][y].color, graphics);
+                this.drawTile_(field[x][y]);
             }
         }
     }
 
-    drawTile_(posit, color, graphics) {// закрашиваем один тайл
-        let coords = this.getCoordsByPosit_(posit);
-        graphics.fillStyle(color);
-        graphics.fillRoundedRect(coords.x + 1, coords.y - 1, this.game.field_.tile.width - 1, this.game.field_.tile.height - 1, 7);
+    drawTile_(field) {// закрашиваем один тайл
+        if (!field) return;
+        let coords = this.getCoordsByPosit_(field.position);
+        this.graphics_.fillStyle(field.color);
+        this.graphics_.fillRoundedRect(coords.x + 1, coords.y + 1, this.game.field_.tile.width - 2, this.game.field_.tile.height - 2, 7);
 
         //--- Прорисовка для всех возможных вариантов ---
     }
@@ -44,8 +49,81 @@ export class Canvas{
     getCoordsByPosit_(posit) {// координаты верхнего правого угла тайла
         let x = posit.x * this.game.field_.tile.width;
         let y = posit.y * this.game.field_.tile.height;
-
         return new Position(x, y);
+    }
+
+    getPositByCoords_(coords) {
+        let x = Math.floor(coords.x / this.game.field_.tile.width);
+        let y = Math.floor(coords.y / this.game.field_.tile.height);
+        return new Position(x, y);
+    }
+
+    onClick_(context) {//передает позицию тайла при клике
+        let position = this.getPositByCoords_(new Position(context.worldX, context.worldY));
+        let neightbors = this.game.field_.onClick(position);
+        if (neightbors) {
+            this.burn_(neightbors, () => {
+                neightbors.forEach((posit) => {
+                    this.game.field_.cells[posit.x][posit.y] = null;
+                })  
+                this.graphics_.clear();
+                this.draw_(this.game.field_.cells); 
+                let cells = this.game.field_.move();
+                this.move_(cells);
+
+            });         
+        }
+    }
+
+    burn_(posits, callback) {
+        let width = this.game.field_.tile.width;
+        let height = this.game.field_.tile.height;
+        let graphics = this.graphics_;
+        let canvas = this;
+
+        this.tweens.addCounter({
+            from: 1,
+            to: Math.min(width, height) / 2 - 4,
+            duration: 900,
+            onUpdate: function (tween) {
+                let t = tween.getValue();
+
+                graphics.clear();
+                canvas.draw_(canvas.game.field_.cells);
+                posits.forEach((posit) => {
+                    let coords = canvas.getCoordsByPosit_(posit);
+
+                    graphics.fillStyle(0x2d2d2d);
+                    graphics.fillRoundedRect(coords.x + 1, coords.y + 1, width - 2, height - 2, 7);
+
+                    graphics.fillStyle(canvas.game.field_.cells[posit.x][posit.y].color);
+                    graphics.fillRoundedRect(
+                        coords.x + t, 
+                        coords.y + t, 
+                        width - t * 2,
+                        height - t * 2, 
+                        7
+                    );
+                })
+            }
+        })
+
+        setTimeout(callback, 1000)
+    }
+
+    move_(field) {
+        let movingSet = [];
+        field.forEach((row, x) => {
+            row.forEach((tile, y) => {
+                if (!tile || !tile.from) return;
+                if ( tile.from.x !== x || tile.from.y !== y ) {
+                    movingSet.push(tile);
+                }
+            })
+        })
+
+        this.graphics_.clear();
+        this.draw_(this.game.field_.cells);
     }
 
 }
--- js/field.js
+++ js/field.js
@@ -1,6 +1,5 @@
 import { Tile } from './tile.js'; 
 import { Position } from './position.js';
-import { Canvas } from './canvas.js';
 
 export class Field {
     constructor (config) { //создание поля 
@@ -44,5 +43,69 @@ export class Field {
     getRandomColor(){
         let index = Math.floor( Math.random() * this.colors.length );
         return this.colors[index];
+    }
+
+    onClick(position) {
+        let neightbors = this.getNeightdors(position);
+        if (neightbors.length < this.minGroup) return;
+        return neightbors;
+    }
+
+    getNeightdors(position) {
+        let tile = this.cells[position.x][position.y];
+        if (!tile) return;
+        let color = tile.color;
+        let neightbors = [];
+        let check = (position) => {
+            if( position.x < 0 || position.y < 0 || position.x > this.cols - 1 || position.y > this.rows - 1 )
+                return;
+            let selectTite = this.cells[position.x][position.y];
+            if ( !selectTite || selectTite.checked || selectTite.color !== color ) 
+                return;
+            neightbors.push(position);
+            selectTite.checked = true;
+            check(new Position(position.x - 1, position.y));
+            check(new Position(position.x + 1, position.y));
+            check(new Position(position.x, position.y - 1));
+            check(new Position(position.x, position.y + 1))
+        }
+        check(position);
+
+        return neightbors;
+    }
+
+    move() {
+
+        let cols = [];
+        this.forCell(point => {
+            if (!cols[point.x]) cols[point.x] = [];
+            cols[point.x][point.y] = this.cells[point.x][point.y];
+        })
+
+        cols.forEach((col, x) => {
+            col.forEach((cell, y) =>{
+                if (cell == null) {
+                    if (y !== 0) {
+                        col.unshift(col.splice(y, 1)[0]); 
+                    }
+                }else{
+                    cell.from = cell.position;
+                }
+            })
+
+            for (let i = 0; i < this.rows; i++) {
+                if (col[i] != null) col[i].position = new Position(x, i);
+            }
+        })
+
+        cols.forEach((col, x) => {
+            col.forEach((cell, y) => {                
+                this.cells[x][y] = cell;
+            })
+        })
+
+        return this.cells;
+
+        //this.canvas.move(this.cells, () => this.canvas.fill())
     }
 }
