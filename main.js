6da468f85485eafffb87907755fc625a9a6f9dd2
LitVi74 <71370749+LitVi74@users.noreply.github.com>
2022-09-05 19:04:13 +0500

    add cocos2d-js

---
+++ main.js
@@ -0,0 +1,18 @@
+
+cc.game.onStart = function(){
+  const size = cc.view.getFrameSize();
+  cc.view.enableRetina(false);
+  cc.view.adjustViewPort(true);
+  cc.view.setDesignResolutionSize(
+    size.width,
+    size.height,
+    cc.ResolutionPolicy.NO_BORDER
+  );
+  cc.view.resizeWithBrowserSize(true);
+
+  //load resources
+  // cc.LoaderScene.preload(Object.values(resources), function () {
+  //     cc.director.runScene(new Game());
+  // }, this);
+};
+cc.game.run();
